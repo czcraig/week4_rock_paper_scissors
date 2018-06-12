2a2d87e70fa8f5303067707a3371778eb82345e6
Iona Macbeth <ionalmacbeth@gmail.com>
2018-06-12 16:32:35 +0100

    mvp complete

---
+++ controller.rb
@@ -0,0 +1,10 @@
+require("sinatra")
+require("sinatra/contrib/all")
+
+require_relative("./models/game.rb")
+
+get "/game/:move1/:move2" do
+  game = Game.new( params[:move1], params[:move2])
+  @game_result = game.play_game
+  erb(:game_result)
+end
--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -3,20 +3,19 @@ require_relative '../models/game.rb'
 
 class TestGame < Minitest::Test
 
-  def setup
-    @game1 = Game.new(@move1, @move2)
-  end
-
   def test_rock_beats_scissors
-    assert_equal(@game1.rock_wins("rock", "scissors"), "Rock wins")
+    game = Game.new("rock", "scissors")
+    assert_equal(game.play_game(), "rock wins")
   end
 
   def test_scissors_beats_paper
-    assert_equal(@game1.scissors_wins("paper", "scissors"), "Scissors wins")
+    game = Game.new("scissors", "paper")
+    assert_equal(game.play_game(), "scissors wins")
   end
 
   def test_draw
-    assert_equal(@game1.draw("paper", "paper"), "It's a draw")
+    game = Game.new("paper", "paper")
+    assert_equal(game.play_game(), "It's a draw!")
   end
 
 end
